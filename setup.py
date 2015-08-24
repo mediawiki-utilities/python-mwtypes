60c3ad08f8bb0d313b9bacc55c3c2cd910bed67d
halfak <aaron.halfaker@gmail.com>
2015-08-24 16:17:43 -0500

    Increments version to 0.1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='mwtypes',
-    version="0.1.0",
+    version="0.1.1",
     author='Aaron Halfaker',
     author_email='aaron.halfaker@gmail.com',
     packages=find_packages(),
